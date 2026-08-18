908c1f2ca0416d280dde21efd6e6e337c896bd86
nirusha-dev <eng25am0284@dsu.edu.in>
2026-08-18 23:17:00 +0530

    Program3_MultiplicationTable.java
    import java.util.Scanner;
    
    public class Program3_ MultiplicationTable {
        public static void main(String[] args) {
    
            Scanner sc = new Scanner(System.in);
    
            System.out.print("Enter a number: ");
            int num = sc.nextInt();
    
            for (int i = 1; i <= 10; i++) {
                System.out.println(num + " x " + i + " = " + (num * i));
            }
    
            sc.close();
        }
    }

--- Program2_LargestOfThree.java
+++ Program2_LargestOfThree.java
@@ -1,6 +1,6 @@
  import java.util.Scanner;
 
-public class LargestOfThree {
+public class Program2_LargestOfThree {
      public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
