8e7217db3a62239eb2eeb332296fee814dfd9e9a
nirusha-dev <eng25am0284@dsu.edu.in>
2026-09-04 21:15:27 +0530

    Longestword.java
    import java.util.Scanner;
    
    public class LongestWord {
        public static void main(String[] args) {
    
            Scanner sc = new Scanner(System.in);
    
            System.out.print("Enter a sentence: ");
            String str = sc.nextLine();
    
            String[] words = str.split(" ");
            String longest = "";
    
            for (String word : words) {
                if (word.length() > longest.length()) {
                    longest = word;
                }
            }
    
            System.out.println("Longest word: " + longest);
    
            sc.close();
        }
    }

---
+++ Longestword.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Longestword {
+    public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+ System.out.print("Enter a sentence: ");
+        String str = sc.nextLine();
+ String[] words = str.split(" ");
+        String longest = "";
+for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+ System.out.println("Longest word: " + longest);
+
+        sc.close();
+    }
+}
