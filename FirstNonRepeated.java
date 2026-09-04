b406c2dad1a49a9c75e574429215d4c2966a6d44
nirusha-dev <eng25am0284@dsu.edu.in>
2026-09-04 19:39:54 +0530

    FirstNonRepeated.java
    import java.util.*;
    
    public class FirstNonRepeated {
        public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
    
            System.out.print("Enter a string: ");
            String str = sc.nextLine();
    
            for (int i = 0; i < str.length(); i++) {
                char ch = str.charAt(i);
    
                if (str.indexOf(ch) == str.lastIndexOf(ch)) {
                    System.out.println("First non-repeated character: " + ch);
                    break;
                }
            }
    sc.close();
        }
    }

---
+++ FirstNonRepeated.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class FirstNonRepeated {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (str.indexOf(ch) == str.lastIndexOf(ch)) {
+                System.out.println("First non-repeated character: " + ch);
+                break;
+            }
+        }
+    }
+}
